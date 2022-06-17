cf459cbb0403f4a6e04e2396bc433122f0506273
MaxiCode <andresholsbach@gmail.com>
2022-06-17 13:09:59 -0300

    Se añade Signupview y login para perfiles

--- Entrevistas/urls.py
+++ Entrevistas/urls.py
@@ -2,9 +2,13 @@ from Entrevistas import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.EntrevistaCreate.as_view(), name="EntrevistaCreate"),
-    path('Listar/',views.EntrevistaList.as_view(), name="EntrevistaList"),
-    path('<pk>/',views.EntrevistaDetail.as_view(), name="EntrevistaDetail"),
+    path('crear/',views.EntrevistaCreate.as_view(), name="EntrevistaCreate"),
+    path('',views.EntrevistaList.as_view(), name="EntrevistaList"),
+    path('detalle/<pk>/',views.EntrevistaDetail.as_view(), name="EntrevistaDetail"),
     path('editar/<pk>/',views.EntrevistaUpdate.as_view(), name="EntrevistaUpdate"),
     path('borrar/<pk>/',views.EntrevistaDelete.as_view(), name="EntrevistaDelete"),
+    path('entrar/',views.EntrevistaLogin.as_view(), name="EntrevistaLogin"),
+    path('salir/',views.EntrevistaLogin.as_view(), name="EntrevistaLogout"),
+    path('crearperfil/',views.SignUpView.as_view(), name="EntrevistaSignUpView"),
 ]
+
--- Entrevistas/views.py
+++ Entrevistas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.forms import UserCreationForm
 from Entrevistas.models import EntrevistasModel
 
 class EntrevistaList(ListView):
@@ -11,17 +17,31 @@ class EntrevistaDetail(DetailView):
     model = EntrevistasModel
     template_name = "Entrevistas/Entrevistasdetail.html"
 
-class EntrevistaCreate(CreateView):
+class EntrevistaCreate(LoginRequiredMixin, CreateView):
     model = EntrevistasModel
     success_url = reverse_lazy("EntrevistaList")
     fields = ["Titulo", "Localidad", "Entrevistado", "Anecdota", "Corresponsal"]
 
-class EntrevistaUpdate(UpdateView):
+class EntrevistaUpdate(LoginRequiredMixin, UpdateView):
     model = EntrevistasModel
     success_url = reverse_lazy("EntrevistaList")
     fields = ["Titulo", "Localidad", "Entrevistado", "Anecdota", "Corresponsal"]
 
-class EntrevistaDelete(DeleteView):
+class EntrevistaDelete(LoginRequiredMixin, DeleteView):
     model = EntrevistasModel
     success_url = reverse_lazy("EntrevistaList")
+
+class EntrevistaLogin(LoginView):
+    template_name = 'Entrevistas/Entrevistaslogin.html'
+    next_page = reverse_lazy("EntrevistaList")
+
+
+class EntrevistaLogout(LogoutView):
+    template_name = 'Entrevistas/Entrevistaslogout.html'
+
+class SignUpView(SuccessMessageMixin, CreateView):
+  template_name = 'Entrevistas/Entrevistas_crear_cuenta.html'
+  success_url = reverse_lazy('EntrevistaLogin')
+  form_class = UserCreationForm
+  success_message = "Tu perfil de corresponsal a sido creado satisfactoriamente."
 # Create your views here.
